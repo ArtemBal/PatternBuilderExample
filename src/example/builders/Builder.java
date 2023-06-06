2e07b5f03b6a89f281333e463d6223fc6fe6de81
Artem Balan <a.s.balan96@gmail.com>
2023-06-06 15:31:37 +0300

    Add builders and products

---
+++ src/example/builders/Builder.java
@@ -0,0 +1,13 @@
+package example.builders;
+
+import example.cars.CarType;
+
+public interface Builder {
+    void setCarType(CarType type);
+    void setSeats(int seats);
+    void setEngine(Engine engine);
+    void setTransmission(Transmission transmission);
+    void setTripComputer(TripComputer tripComputer);
+    void setGPSNavigator(GPSNavigator gpsNavigator)
+
+}
